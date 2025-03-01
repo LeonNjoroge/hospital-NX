457054611a0fb39c057ef7cca75165cceb9ebd96
LeonNjoroge <leonmuraguri@gmail.com>
2025-03-01 16:47:09 +0300

    Create Dashboard component

---
+++ apps/host/src/dashboard/dashoard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [CommonModule],
+  templateUrl: './dashoard.component.html',
+  styleUrl: './dashoard.component.css',
+})
+export class DashboardComponent {}
