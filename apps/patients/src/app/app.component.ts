d04f2959749f31013b9eab6a19d5516ead4e097f
LeonNjoroge <leonmuraguri@gmail.com>
2025-03-02 10:25:18 +0300

    add nav bar and assets folder to mfes

--- apps/patients/src/app/app.component.ts
+++ apps/patients/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from '@hospital-common/libs';
 
 @Component({
-  imports: [ RouterModule],
+  imports: [ RouterModule, NavbarComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
